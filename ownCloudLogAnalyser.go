9dbfaf305f5fa3d0a5af22b97562c946a49d1dc8
Artur Neumann <info@individual-it.net>
2018-10-18 17:25:08 +0545

    allow to view only specific keys from the JSON log string

--- ownCloudLogAnalyser.go
+++ ownCloudLogAnalyser.go
@@ -6,11 +6,14 @@ import (
     "log"
     "os"
     "bufio"
+    "strings"
+    "encoding/json"
 )
 
 func main() {
     fileNamePtr := flag.String("f", "", "the ownCloud log file")
     showLineCounterPtr := flag.Bool("linenumbers", false, "show the line numbers")
+    listOfKeysToViewStringPtr := flag.String("view", "", "list of keys to be shown (separate by comma), if empty all are shown")
     flag.Parse()
 
     logFile, err := os.Open(*fileNamePtr)
@@ -23,9 +26,33 @@ func main() {
 
     logFileScanner := bufio.NewScanner(logFile)
     lineCounter := 1
+    needJsonDecode := false
+    
+    //split the list of keys to view into slices
+    listOfKeysToView := make([]string,0)
+    if *listOfKeysToViewStringPtr != "" {
+       listOfKeysToView = strings.Split(*listOfKeysToViewStringPtr, ",")
+       needJsonDecode = true
+    }
+
+    //main loop to loop through the log file
     for logFileScanner.Scan() {
+        
+        //parse unstructured json
+        //see https://www.sohamkamani.com/blog/2017/10/18/parsing-json-in-golang/
+        var result map[string]interface{}
+        if needJsonDecode {
+            json.Unmarshal([]byte(logFileScanner.Text()), &result)
+        }
         if *showLineCounterPtr {
-            fmt.Printf("%v: %v\n", lineCounter, logFileScanner.Text())
+            fmt.Printf("%v: ", lineCounter)
+        }
+        if len(listOfKeysToView) > 0 {
+            //show only the keys we requested
+            for _, jsonKey := range listOfKeysToView {
+                fmt.Printf("%v: %v\t", jsonKey, result[jsonKey])
+            }
+            fmt.Println()
         } else {
             fmt.Println(logFileScanner.Text())
         }
