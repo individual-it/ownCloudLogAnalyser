9a401ca9dce06dc86311d649095dbc7ae89c0cb5
Artur Neumann <info@individual-it.net>
2018-10-18 15:24:10 +0545

    read the file and print its content

--- ownCloudLogAnalyser.go
+++ ownCloudLogAnalyser.go
@@ -1,10 +1,31 @@
 package main
 
-import "fmt"
-import "flag"
+import (
+    "fmt"
+    "flag"
+    "log"
+    "os"
+    "bufio"
+)
 
 func main() {
     fileNamePtr := flag.String("f", "", "the ownCloud log file")
     flag.Parse()
-    fmt.Println("filename:", *fileNamePtr)
+
+    logFile, err := os.Open(*fileNamePtr)
+    if err != nil {
+        log.Fatal(err)
+    }
+    //make sure the file is closed also when somethig fails
+    //see https://blog.golang.org/defer-panic-and-recover
+    defer logFile.Close()
+
+    logFileScanner := bufio.NewScanner(logFile)
+    for logFileScanner.Scan() {
+        fmt.Println(logFileScanner.Text())
+    }
+
+    if err := logFileScanner.Err(); err != nil {
+        log.Fatal(err)
+    }
 }
