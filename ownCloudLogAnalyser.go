c8f16f5baf8cc76815f9257d03301a37607c7cdc
Artur Neumann <info@individual-it.net>
2018-10-19 12:11:10 +0545

    allow filter results

--- ownCloudLogAnalyser.go
+++ ownCloudLogAnalyser.go
@@ -9,12 +9,14 @@ import (
     "strings"
     "encoding/json"
     "strconv"
+    "gopkg.in/Knetic/govaluate.v3"
 )
 
 func main() {
     fileNamePtr := flag.String("f", "", "the ownCloud log file")
     showLineCounterPtr := flag.Bool("linenumbers", false, "show the line numbers")
     listOfKeysToViewStringPtr := flag.String("view", "", "list of keys to be shown (separate by comma), if empty all are shown")
+    filterPtr := flag.String("filter", "", "filter the output by logical expressions e.g. \"user=='admin'&&level>=3\"")
     flag.Parse()
 
     logFile, err := os.Open(*fileNamePtr)
@@ -28,37 +30,50 @@ func main() {
     logFileScanner := bufio.NewScanner(logFile)
     lineCounter := 1
     needJsonDecode := false
-    
+
     //split the list of keys to view into slices
     listOfKeysToView := make([]string,0)
+    if *listOfKeysToViewStringPtr != "" || *filterPtr != "" {
+       needJsonDecode = true
+    }
+
     if *listOfKeysToViewStringPtr != "" {
        listOfKeysToView = strings.Split(*listOfKeysToViewStringPtr, ",")
-       needJsonDecode = true
     }
 
     //main loop to loop through the log file
     for logFileScanner.Scan() {
-        
         //parse unstructured json
         //see https://www.sohamkamani.com/blog/2017/10/18/parsing-json-in-golang/
-        var result map[string]interface{}
+        var decodedData map[string]interface{}
         if needJsonDecode {
-            err := json.Unmarshal([]byte(logFileScanner.Text()), &result)
+            err := json.Unmarshal([]byte(logFileScanner.Text()), &decodedData)
             if err != nil {
                 log.Fatal("JSON error, line: ", strconv.Itoa(lineCounter), " error: ", err)
             }
         }
-        if *showLineCounterPtr {
-            fmt.Printf("%v: ", lineCounter)
+        filterExpression, err := govaluate.NewEvaluableExpression(*filterPtr);
+        if err != nil {
+            log.Fatal("cannot evaluate filter string: ", err)
         }
-        if len(listOfKeysToView) > 0 {
-            //show only the keys we requested
-            for _, jsonKey := range listOfKeysToView {
-                fmt.Printf("%v: %v\t", jsonKey, result[jsonKey])
+        filterResult, err := filterExpression.Evaluate(decodedData);
+        if err != nil {
+            log.Fatal("cannot evaluate filter string: ", err)
+        }
+
+        if filterResult==true {
+            if *showLineCounterPtr {
+                fmt.Printf("%v: ", lineCounter)
             }
-            fmt.Println()
-        } else {
-            fmt.Println(logFileScanner.Text())
+            if len(listOfKeysToView) > 0 {
+                //show only the keys we requested
+                for _, jsonKey := range listOfKeysToView {
+                    fmt.Printf("%v: %v\t", jsonKey, decodedData[jsonKey])
+                }
+                fmt.Println()
+            } else {
+                fmt.Println(logFileScanner.Text())
+            }
         }
         lineCounter++
     }
